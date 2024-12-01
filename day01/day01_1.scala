a26351fbb6a62c99aee5789e486c19629ded845b
Pineapple217 <rosierjules@gmail.com>
2024-12-01 10:48:37 +0100

    day 1

---
+++ day01/day01_1.scala
@@ -0,0 +1,21 @@
+import scala.io.Source
+import scala.util.Using
+
+object Main extends App {
+    val lines = Using.resource(Source.fromFile("./input.txt")) { file =>
+        file.getLines().toVector
+    }
+
+    val (input1, input2) = lines.map { line =>
+        val Array(first, second) = line.split(" {3}").map(_.trim.toInt)
+        (first, second)
+    }.unzip
+
+    val result = input1
+        .sorted
+        .zip(input2.sorted)
+        .map { case (a, b) => (a - b).abs }
+        .sum
+
+    println(result)
+}
