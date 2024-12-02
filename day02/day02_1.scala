b7d6eb9ea692bc2ebf564553c3ea768edb30032e
Pineapple217 <rosierjules@gmail.com>
2024-12-02 11:44:32 +0100

    day 2

---
+++ day02/day02_1.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+import scala.util.Using
+
+object Main extends App {
+    val lines = Using.resource(Source.fromFile("./input.txt")) { file =>
+        file.getLines()
+            .map(_.split("\\s+")
+            .map(_.toInt)
+            .toVector)
+            .toVector
+    }
+
+    val result = lines.map(line => {
+            val difs = line.sliding(2).collect {
+                case Seq(a, b) => b - a
+            }.toVector
+            difs.forall(x => x.abs > 0 && x.abs <= 3) &&
+            (difs.forall(_ > 0) || difs.forall(_ < 0))
+    }).count(identity)
+
+    println(result)
+}
